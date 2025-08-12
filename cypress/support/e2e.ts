2d34b28dd8b0bac4dfdaac6cbb4319a8ea89a57c
sgarretson <scott@brilliantchemistry.com>
2025-08-12 00:03:13 -0400

    test(e2e): Use programmatic NextAuth login to eliminate input flakiness

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,22 +1,32 @@
 import 'cypress-axe';
 
+// Programmatic login via NextAuth credentials provider to avoid flakiness
 Cypress.Commands.add('login', (email: string, password: string) => {
   cy.session([email, password], () => {
-    cy.visit('/auth');
-    cy.get('#email', { timeout: 10000 })
-      .should('be.visible')
-      .then(($el) => {
-        cy.wrap($el).invoke('val', '').trigger('input');
-        cy.wrap($el).invoke('val', email).trigger('input');
+    // Get CSRF token
+    cy.request('GET', '/api/auth/csrf').then((res) => {
+      const csrfToken = res.body?.csrfToken;
+      expect(csrfToken, 'csrfToken').to.be.a('string').and.to.have.length.greaterThan(0);
+      // Submit credentials to NextAuth callback
+      cy.request({
+        method: 'POST',
+        url: '/api/auth/callback/credentials',
+        form: true,
+        body: {
+          csrfToken,
+          email,
+          password,
+          callbackUrl: '/',
+          json: 'true',
+        },
+        followRedirect: false,
+      }).then((resp) => {
+        // Should set auth cookies; navigate to home
+        expect([200, 302, 303], 'auth response status').to.include(resp.status);
+        cy.visit('/');
+        cy.location('pathname', { timeout: 10000 }).should('not.include', '/auth');
       });
-    cy.get('#password')
-      .should('be.visible')
-      .then(($el) => {
-        cy.wrap($el).invoke('val', '').trigger('input');
-        cy.wrap($el).invoke('val', password).trigger('input');
-      });
-    cy.get('button[type="submit"]').click();
-    cy.location('pathname', { timeout: 10000 }).should('not.include', '/auth');
+    });
   });
 });
 
